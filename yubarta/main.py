6fc49fec4d7ec2fc37b54456049f5c7b0b9da0a3
Diego Fernando Rojas <rojastorrado@gmail.com>
2025-04-01 17:58:44 -0500

    Upgrade tp Py3.13

--- yubarta/main.py
+++ yubarta/main.py
@@ -5,7 +5,6 @@ from yubarta.drivers.db.orm import start_mappers
 from yubarta.entrypoints.api_server.v1.router import router as v1_router
 from sqlalchemy.ext.asyncio import AsyncSession
 
-print("HOLAAAAA")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
