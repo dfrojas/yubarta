ab078e06e7e747725d9078d5c3de9248cfc4a72e
Diego Fernando Rojas <rojastorrado@gmail.com>
2026-09-22 20:13:03 +0200

    chore: Remove legacy code

--- legacy/yubarta/ingestion/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .routes import z
-from .routes.webhook import router as webhook_router
-
-router = APIRouter(prefix="/api/v1")
-
-router.include_router(z.router)
-router.include_router(webhook_router, prefix="/webhook")
